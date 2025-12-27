d3f19932c0ad9aa2dcfdc959d2f26111761f033f
Cabdikariim <cabdikariimmaxamaddaahir@gmail.com>
2025-12-27 09:40:57 +0300

    made the app starters

---
+++ app/Auth/signUp.tsx
@@ -0,0 +1,18 @@
+import { View, Text ,ScrollView} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const signUp = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+      <Text>signUp</Text>
+    </View>
+      </ScrollView>
+    </SafeAreaView>
+    
+  )
+}
+
+export default signUp
